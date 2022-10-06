6df31caf3c6ee8796dc434e6616acdd64186da6c
khaleghi78 <khaleghirt@gmail.com>
2022-10-06 16:36:57 +0330

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import { ChatEngine } from "react-chat-engine";
-import './App.css';
+import { ChatEngine } from 'react-chat-engine';
+
 import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '7fcce73a-8b38-40e4-a026-62d313fb4212';
 
 const App = () => {
-if(!localStorage.getItem('username')) return <LoginForm />
+  if (!localStorage.getItem('username')) return <LoginForm />;
 
-
-    return (
-    <ChatEngine 
-       height="100vh"
-       projectID="7fcce73a-8b38-40e4-a026-62d313fb4212"
-       userName={localStorage.getItem('username')}
-       userSecret={localStorage.getItem('password')}
-       renderChatFeed={(chatAppProps) => <ChatFeed { ... chatAppProps } /> }
-       onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
+  );
+};
 
-    );
-}
+// infinite scroll, logout, more customizations...
 
 export default App;
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 
-const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+const projectID = '7fcce73a-8b38-40e4-a026-62d313fb4212';
 
 const Modal = () => {
   const [username, setUsername] = useState('');
